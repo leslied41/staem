768a466a2cab7d1579f0d633b4bac883786f4a36
leslieduan <zduan@utas.edu.au>
2022-09-27 10:29:40 +1000

    add spinner loader in carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,39 +1,48 @@
-import { useEffect } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { useData, useBreakPoints } from "../../hooks";
+import BeatLoader from "react-spinners/BeatLoader";
+
 import "@splidejs/react-splide/css";
 
 const Carousel = () => {
   const { data, isLoading } = useData();
   const { isMobile } = useBreakPoints();
-  return (
-    <Splide
-      aria-label="My Favorite Images"
-      options={{
-        type: "loop",
-        rewind: true,
-        gap: "1rem",
-        perMove: 1,
-        perPage: 1,
-        drag: isMobile ? true : false,
-        focus: "center",
-        isNavigation: "true",
-        classes: {
-          arrow: "hidden",
-          pagination: "custom-pagination",
-        },
-      }}
-    >
-      {data?.body.map((game) => {
-        const { id, link, image, price, title, genre, tags } = game;
-        return (
-          <SplideSlide key={id}>
-            <img src={image} alt={title} />
-          </SplideSlide>
-        );
-      })}
-    </Splide>
-  );
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center">
+        <BeatLoader />
+      </div>
+    );
+  else
+    return (
+      <Splide
+        aria-label="My Favorite Images"
+        options={{
+          type: "loop",
+          rewind: true,
+          gap: "1rem",
+          perMove: 1,
+          perPage: 1,
+          drag: isMobile ? true : false,
+          focus: "center",
+          isNavigation: "true",
+          classes: {
+            arrow: "hidden",
+            pagination: "custom-pagination",
+          },
+        }}
+      >
+        {data?.body.map((game) => {
+          const { id, link, image, price, title, genre, tags } = game;
+          return (
+            <SplideSlide key={id}>
+              <img src={image} alt={title} />
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+    );
 };
 
 export default Carousel;
